e53030a42987b7dc0a644812b7fe1ec0b64090c6
Mia Kang <miakangnyc@gmail.com>
2022-01-27 18:34:13 -0500

    working input field displaying new p tags

--- assets/app.js
+++ assets/app.js
@@ -1,27 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello');
-
-  // const confirmSpace = document.createElement('div');
-  const testButton = document.querySelector('#button');
-  const clickList = document.getElementById('click-list');
+  // console.log('Hello');
+  const fruitInput = document.getElementById('fruitInput');
+  const testButton = document.getElementById('addButton');
+  const fruitList = document.getElementById('fruitList');
 
   testButton.addEventListener('click', function () {
     const clickConfirm = document.createElement('p');
-    clickConfirm.innerText = 'Button was clicked';
-    clickList.appendChild(clickConfirm);
+    clickConfirm.innerText = fruitInput.value;
+    fruitList.appendChild(clickConfirm);
   });
 });
-
-// console.log('hello');
-
-// document.addEventListener('DOMContentLoaded', (e) => {
-//   console.log('hello again');
-
-//   const testButton = document.querySelector('#button');
-
-//   testButton.addEventListener('click', buttonClicked);
-
-//   function buttonClicked() {
-//     window.alert('Button was clicked!');
-//   }
-// });
